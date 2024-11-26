6c1d4258fffe874c4a6fc36fa68a2644d083a86e
Thái Vĩnh Đạt <tvdat20004@gmail.com>
2024-11-26 18:20:18 +0700

    Delete demo directory

--- demo/backend/routes/security/operatorInjection/constants.js
+++
@@ -1,8 +0,0 @@
-// Sử dụng một số route name khó đoán
-const ROUTES = {
-    ADMIN: '/d2ad517e6b7b', // Mã hóa của /admin-dashboard
-    USER: '/u8x9v2p4m5n',  // Mã hóa của /user-dashboard
-    LOGIN: '/auth'
-};
-
-module.exports = ROUTES;
--- demo/frontend/src/components/tests/OperatorInjection/config/routes.js
+++
@@ -1,7 +0,0 @@
-// Phải giống với routes trong backend
-export const ROUTES = {
-    ADMIN: '/test/operator-injection/d2ad517e6b7b',
-    USER: '/test/operator-injection/u8x9v2p4m5n',
-    LOGINVUL: '/test/operator-injection/vulnerable',
-    LOGINSEC: '/test/operator-injection/secure'
-};
